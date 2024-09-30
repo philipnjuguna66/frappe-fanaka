dd441e0634457151d56553a20435e68f23e94662
Philip Njuguna <philipnjuguna66@gmail.com>
2024-09-30 10:04:25 +0300

    feat: lead status module

---
+++ fanaka_app/fanaka_app/doctype/lead_status/lead_status.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Philip Njuguna and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Lead Status", {
+// 	refresh(frm) {
+
+// 	},
+// });
