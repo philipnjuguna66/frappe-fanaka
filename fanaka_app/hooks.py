67e6a47aab0c5160a4d467b0c64a663d0a6ffbf2
Philip Njuguna <philipnjuguna66@gmail.com>
2024-10-28 13:30:03 +0300

    feat: lead status module

--- fanaka_app/hooks.py
+++ fanaka_app/hooks.py
@@ -165,10 +165,6 @@ app_license = "mit"
 
 # hooks.py
 
-# hooks.py in fanaka_app
-override_whitelisted_methods = {
-    "frappe.integrations.oauth2_logins.custom.fanaka_": "fanaka_app.api.oauth_custom.login_via_fanaka_oauth"
-}
 
 
 
