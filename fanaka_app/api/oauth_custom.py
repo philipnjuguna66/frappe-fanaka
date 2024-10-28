2bce869551b198b87f6e2a83f7782cb98ed620ed
Philip Njuguna <philipnjuguna66@gmail.com>
2024-10-28 12:50:23 +0300

    feat: lead status module

--- fanaka_app/api/oauth_custom.py
+++ fanaka_app/api/oauth_custom.py
@@ -1,11 +1,11 @@
 import frappe
-from frappe.integrations.oauth2_logins import login_via_oauth2
+from frappe.integrations.oauth2_logins import login_via_oauth2, decoder_compat
 
 @frappe.whitelist(allow_guest=True)
 def login_via_custom_oauth(code=None, state=None):
     try:
         # Attempt to log in via Fanaka_ OAuth provider
-        login_via_oauth2("fanaka_", code, state)
+        login_via_oauth2("fanaka_", code, state ,decoder=decoder_compat)
     except frappe.exceptions.ValidationError as e:
         # Check if the error is due to unverified email
         if "Email not verified" in str(e):
