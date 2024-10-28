74335ce66031769fe35a6ce0647f126eb12e2c97
Philip Njuguna <philipnjuguna66@gmail.com>
2024-10-28 12:31:22 +0300

    feat: lead status module

--- fanaka_app/helpers.py
+++ fanaka_app/helpers.py
@@ -8,8 +8,4 @@ def update_clearance_date(journal_name):
     journal = frappe.get_doc('Journal Entry', journal_name)
     journal.db_set("clearance_date", journal.posting_date)
 
-@frappe.whitelist(allow_guest=True)
-def login_via_custom_oauth(code=None, state=None):
-    login_via_oauth2("CustomOAuth", code, state)
-
 
