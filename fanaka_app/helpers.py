c5f29d340e9ae8583a21f1e60548979bcdc7f718
Philip Njuguna <philipnjuguna66@gmail.com>
2024-10-28 12:18:48 +0300

    feat: lead status module

--- fanaka_app/helpers.py
+++ fanaka_app/helpers.py
@@ -1,6 +1,6 @@
 import frappe
 
-from frappe.integrations.oauth2_logins import login_via_oauth2
+from frappe.integrations.oauth2_logins import custom
 
 
 @frappe.whitelist()
@@ -10,4 +10,4 @@ def update_clearance_date(journal_name):
 
 @frappe.whitelist(allow_guest=True)
 def login_via_custom_oauth(code=None, state=None):
-    login_via_oauth2("Fanaka App", code, state)
+    custom(code, state)
