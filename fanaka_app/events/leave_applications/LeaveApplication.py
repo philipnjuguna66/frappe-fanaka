141b4780b277cf59c9f2b67b96878eeaa735db22
Philip Njuguna <philipnjuguna66@gmail.com>
2025-01-08 11:00:08 +0300

    feat: validate leave application requirements

--- fanaka_app/events/leave_applications/LeaveApplication.py
+++ fanaka_app/events/leave_applications/LeaveApplication.py
@@ -4,12 +4,10 @@ from frappe.utils import date_diff, today
 def pass_requirement(doc, event):
     if doc.get('doctype') == 'Leave Application':
 
-        # Validate that the leave type is "Annual Leave"
-        if doc.leave_type != "Annual Leave":
-            return
+
 
         # Validate leave application date (at least 3 days in advance)
-        if doc.from_date:
+        if doc.leave_type === "Annual leave" && doc.from_date:
             days_to_leave_start = date_diff(doc.from_date, today())
             if days_to_leave_start < 3:
                 frappe.throw("You can only apply for Annual Leave at least 3 days in advance.")
