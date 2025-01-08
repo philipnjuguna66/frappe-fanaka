2d1c167a0206da36a4b2e3a97ffae1c5267209fb
Philip Njuguna <philipnjuguna66@gmail.com>
2025-01-08 11:59:24 +0300

    feat: validate leave application requirements

---
+++ fanaka_app/events/leave_applications/Leave_application.py
@@ -0,0 +1,25 @@
+import frappe
+from frappe.utils import date_diff, today
+
+
+def pass_requirement(doc, event):
+    frappe.throw(doc.leave_type)
+
+    if (doc.get('leave_type') == "Annual Leave"):
+        if doc.from_date:
+            days_to_leave_start = date_diff(doc.from_date, today())
+        if days_to_leave_start < 3:
+            frappe.throw("You can only apply for Annual Leave at least 3 days in advance.")
+
+    # Check for existing leave applications in "Draft" status
+    existing_draft_leaves = frappe.db.count(
+        'Leave Application',
+        filters={
+            'employee': doc.employee,
+            'status': 'Open'
+        }
+    )
+
+    if existing_draft_leaves >= 2:
+        frappe.throw(
+            "You cannot create a third Annual Leave application while previous ones are still in 'Open' status.")
