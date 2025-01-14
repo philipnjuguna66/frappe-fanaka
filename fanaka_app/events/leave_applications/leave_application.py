e6ab255cecd00905a5fde30ab6ee0ebbaeeb4c26
Philip Njuguna <philipnjuguna66@gmail.com>
2025-01-14 11:31:34 +0300

    feat: validate leave application requirements

--- fanaka_app/events/leave_applications/leave_application.py
+++ fanaka_app/events/leave_applications/leave_application.py
@@ -2,12 +2,12 @@ import frappe
 from frappe.utils import date_diff, today
 def pass_requirement(doc, event):
     try:
-        frappe.log_error(f"leave application validations triggered for {doc.doctype} | Name: {doc.name}", "Custom Hook Log")
-        if (doc.leave_type == "Annual Leave"):
+        frappe.log_error(f"leave application validations triggered for {doc.leave_type} | Name: {doc.name}", "Custom Hook Log")
+        if (doc.leave_type.upper() == "ANNUAL LEAVE"):
             if doc.from_date:
                 days_to_leave_start = date_diff(doc.from_date, today())
-        if days_to_leave_start < 3:
-            frappe.throw("You can only apply for Annual Leave at least 3 days in advance.")
+                if days_to_leave_start < 3:
+                    frappe.throw("You can only apply for Annual Leave at least 3 days in advance.")
 
         # Check for existing leave applications in "Draft" status
         existing_draft_leaves = frappe.db.count(
@@ -17,10 +17,8 @@ def pass_requirement(doc, event):
                 'status': 'Open'
             }
         )
-
-        if existing_draft_leaves >= 1:
-            frappe.throw(
-                "You cannot create a third Annual Leave application while previous ones are still in 'Open' status.")
+        if existing_draft_leaves >= 2:
+            frappe.throw("You cannot create a third Annual Leave application while previous ones are still in 'Open' status.")
     except Exception as e:
         frappe.log_error(f"Error in custom_before_save: {str(e)}", "Custom Hook Error")
         raise
