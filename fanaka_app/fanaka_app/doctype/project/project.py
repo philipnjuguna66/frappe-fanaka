a102f6cb7e56a9f2a6ede27c5ed22c35b84abd6a
Philip Njuguna <philipnjuguna66@gmail.com>
2025-03-19 13:30:24 +0300

    chore: update project creation

--- fanaka_app/fanaka_app/doctype/project/project.py
+++ fanaka_app/fanaka_app/doctype/project/project.py
@@ -1,24 +1,24 @@
 import frappe
 from frappe.utils import flt
 
+
 @frappe.whitelist()
 def subdivide_plots(project, no_of_plots, plot_size, price_per_plot):
     # Loop to create individual plot records
     for i in range(1, int(no_of_plots) + 1):
         plot = frappe.get_doc({
             "doctype": "Plot",
-            "plot_no":  {i},
+            "plot_no": {i},
             "size": flt(plot_size),
             "project": project,  # Link to the parent Project
             "parent": project,  # Link to the parent Project
-            "parenttype": "Project",   # Link to the parent Project
-            "parentfield": "custom_project_plots",   # Link to the parent Project
+            "parenttype": "Project",  # Link to the parent Project
+            "parentfield": "custom_project_plots",  # Link to the parent Project
             "status": "Available",  # Default status
             "price": flt(price_per_plot),  # Price per plot
 
         })
         plot.insert()  # Save the plot record
-
 
         item_code = plot.name
 
@@ -35,7 +35,6 @@ def subdivide_plots(project, no_of_plots, plot_size, price_per_plot):
             })
 
         item.insert()
-
 
         # Create Stock Entry for the plot
         stock_entry = frappe.get_doc({
