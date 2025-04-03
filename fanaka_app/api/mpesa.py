b109b1377c9431ba045e2b0397e06eb754400761
Philip Njuguna <philipnjuguna66@gmail.com>
2025-04-03 15:59:23 +0300

    chore: update project creation

--- fanaka_app/api/mpesa.py
+++ fanaka_app/api/mpesa.py
@@ -2,21 +2,21 @@ import frappe
 import base64
 import json
 import requests
-from frappe.utils import now
 import os
 from pathlib import Path
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.hashes import SHA1
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
 
-
+# Load the environment variables
 @frappe.whitelist()
 def get_access_token():
-    consumer_key = os.getenv("MPESA_BUSINESS_CONSUMER_KEY")
-    consumer_secret = os.getenv("MPESA_BUSINESS_CONSUMER_SECRET")
+    consumer_key =   os.environ.get("MPESA_BUSINESS_CONSUMER_KEY")
+    consumer_secret =  os.environ.get("MPESA_BUSINESS_CONSUMER_SECRET")
+
     response = requests.get(
         "https://api.safaricom.co.ke/oauth/v1/generate",
         auth=(consumer_key, consumer_secret),
         params={"grant_type": "client_credentials"},
     )
-    return response
+    return response.json()['access_token']
