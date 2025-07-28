5a824bf648a9b6c1e28c35ad8dfabbb705672b52
Philip N <philipnjuguna66@gmail.com>
2025-07-28 14:31:09 +0300

    fixL projects and plots update

--- fanaka_app/fanaka_app/doctype/plot/plot.js
+++ fanaka_app/fanaka_app/doctype/plot/plot.js
@@ -6,12 +6,26 @@ frappe.ui.form.on("Plot", {
         if (!frm.doc.__islocal) { // Check if the document has been saved
             frm.disable_save(); // This will disable the save button for new documents
         }
+
+        console.log(frm)
+
         console.log(frappe.ui.toolbar);
         // To hide the 'New' button from the list view.
         // This method is for list view buttons.
         if (frappe.route_options.view === 'List') {
 
-            frappe.ui.toolbar.get_primary_action_button().hide();
+            frappe.ui.toolbar.get_primary_btn().hide();
         }
 	},
 });
+frappe.listview_settings['Plot'] = {
+    refresh: function(listview) {
+        console.log(listview)
+        // Condition: Hide the button if a certain field in a DocType is 'Disabled'.
+        // Example: If the 'allow_creation' field in the 'System Settings' DocType is unchecked.
+        // You would need to fetch the value of this field first.
+
+        // This is the most reliable way to remove the button.
+        listview.page.wrapper.find('.btn-primary[data-action="new"]').hide();
+    }
+};
