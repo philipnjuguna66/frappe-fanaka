03bf714798cbdda65bdb4bf8f64a19b7b16e3f5e
Philip N <philipnjuguna66@gmail.com>
2026-02-26 14:17:10 +0300

    git: completion agreement settings

---
+++ fanaka_app/fanaka_app/doctype/completion_agreement_template/completion_agreement_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Philip Njuguna and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Completion Agreement Template", {
+// 	refresh(frm) {
+
+// 	},
+// });
