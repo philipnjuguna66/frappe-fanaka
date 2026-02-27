e4c476c83fb42f1bdc09ebe2f32fe112f7bb6bfe
Philip Njuguna <philipnjuguna66@gmail.com>
2026-02-27 15:31:37 +0300

    create webinar lead
    Signed-off-by: Philip Njuguna <philipnjuguna66@gmail.com>

--- fanaka_app/api/webinar.py
+++ fanaka_app/api/webinar.py
@@ -18,7 +18,7 @@ def create_lead():
 
     create_lead = frappe.get_doc({
         "doctype": "Webinar Attendance",
-        "name1": data.get("name"),
+        "lead_name": data.get("name"),
         "email": data.get("email"),
         "phone_number": data.get("phone"),
         #"country": data.get("country"),
