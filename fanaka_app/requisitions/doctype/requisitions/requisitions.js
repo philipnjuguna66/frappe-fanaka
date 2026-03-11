4db317baacff17c9256cc58b20a956e7915e60e7
Philip Njuguna <philipnjuguna66@gmail.com>
2026-03-11 20:11:39 +0300

    Update requisitions.js

--- fanaka_app/requisitions/doctype/requisitions/requisitions.js
+++ fanaka_app/requisitions/doctype/requisitions/requisitions.js
@@ -3,32 +3,27 @@
 
 frappe.ui.form.on("Requisitions", {
     refresh: function(frm) {
-        if (frm.doc.docstatus === 1) { // docstatus 1 means submitted
+        if (frm.doc.docstatus === 1 && frm.doc.journal_entry) {
             frm.add_custom_button(__('View Ledger'), function() {
-                if (frm.doc.journal_entry) {
+                // Fetch the JE company to ensure the report filters correctly
+                frappe.db.get_value('Journal Entry', frm.doc.journal_entry, 'company', (r) => {
                     frappe.set_route('query-report', 'General Ledger', {
                         voucher_no: frm.doc.journal_entry,
-                        company: frm.doc.company // Pass company for context if needed
+                        company: r.company
                     });
-                } else {
-                    frappe.msgprint(__('No Journal Entry linked to this transaction.'));
-                }
+                });
             }, __("View"));
         }
-    },
+    }
 });
 
 frappe.listview_settings['Requisitions'] = {
     add_fields: ["requisition_owner"],
-    onload: function(listview) {
-        listview.page.fields_dict['requisition_owner'].get_query = function() {
-            return { filters: { enabled: 1 } };
-        };
-    },
     formatters: {
         requisition_owner(val, df, doc) {
-            // This replaces the email with the Full Name in the list UI without changing data
-            return frappe.user.full_name(val);
+            // Using frappe.user.full_name is correct for current user, 
+            // but for any user ID, we should use the cache or a quick map
+            return frappe.utils.get_fullname(val);
         }
     }
 };
