8cda280a17d965c66c367bb3363229f4d7b0cf72
Philip Njuguna <philipnjuguna66@gmail.com>
2026-03-12 13:07:29 +0300

    Update lead.js

--- fanaka_app/public/js/lead.js
+++ fanaka_app/public/js/lead.js
@@ -2,29 +2,42 @@ frappe.ui.form.on("Lead", {
 
     refresh(frm) {
 
-        if (frm.doc.phone) {
+        frm.add_custom_button("Call Customer", () => {
 
-            frm.add_custom_button("Call Customer", () => {
-
-                frappe.call({
-                    method: "fanaka_app.api.call.make_call",
-                    args: {
-                        phone_number: frm.doc.phone
-                    },
-                    callback: function(r) {
-
-                        if (r.message.status === "success") {
-                            frappe.msgprint("Call initiated successfully");
-                        } else {
-                            frappe.msgprint("Call failed");
-                        }
-
+            frappe.prompt(
+                [
+                    {
+                        label: "Phone Number",
+                        fieldname: "phone_number",
+                        fieldtype: "Data",
+                        reqd: 1,
+                        default: frm.doc.phone || ""
                     }
-                });
+                ],
+                function(values) {
 
-            });
+                    frappe.call({
+                        method: "fanaka_app.api.call.make_call",
+                        args: {
+                            phone_number: values.phone_number
+                        },
+                        callback: function(r) {
 
-        }
+                            if (r.message && r.message.status === "success") {
+                                frappe.msgprint("📞 Call initiated successfully");
+                            } else {
+                                frappe.msgprint("Call failed");
+                            }
+
+                        }
+                    });
+
+                },
+                "Enter Phone Number",
+                "Call"
+            );
+
+        });
 
     }
 
